24d10c89b92a1e04c6ed5959db2973e9ede8c35f
j19015 <j19015@sangi.jp>
2024-03-12 04:03:17 +0900

    feat ポケモンのデータを取得するための関数を追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import getAllPokemon from './utils/pokemon.js';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンのデータを取得
+      const res = await getAllPokemon(initialURL);
+      // eslint-disable-next-line no-console
+      console.log(res);
+    };
+    fetchPokemonData();
+  });
+  return <div className="App" />;
 }
 
-export default App
+export default App;
