fd762de7d7b2d9c46246a58e35f3cafeacd8a34c
j19015 <j19015@sangi.jp>
2024-03-12 04:03:17 +0900

    feat ポケモンのデータを取得するための関数を追加

---
+++ src/utils/pokemon.js
@@ -0,0 +1,9 @@
+const getAllPokemon = (url) =>
+  new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+
+export default getAllPokemon;
