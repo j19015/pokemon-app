000b7268f86eb3d249e68511fb0a06e1fbe46b81
j19015 <j19015@sangi.jp>
2024-03-13 00:39:27 +0900

    feat ローディング機能を追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import getAllPokemon from './utils/pokemon.js';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -11,10 +12,11 @@ function App() {
       const res = await getAllPokemon(initialURL);
       // eslint-disable-next-line no-console
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
-  });
-  return <div className="App" />;
+  }, []);
+  return <div className="App">{loading ? <h1>ロード中</h1> : <h1>データ取得完了</h1>}</div>;
 }
 
 export default App;
